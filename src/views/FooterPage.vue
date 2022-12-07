3d9a49e76195ec5d5f1a4540c33ede5bd5b4c2b2
amart672 <amart672@stu.southtexascollege.edu>
2022-12-06 21:30:26 -0600

    added phone number on footer

--- src/views/FooterPage.vue
+++ src/views/FooterPage.vue
@@ -1,9 +1,10 @@
 <template>
   <footer class="fixed left-0 bottom-0 w-full bg-red-300">
     <p class="text-center text-lg text-slate-800">Contact Us</p>
-    <RouterLink to="/facebook" class="text-m text-start text-sky-800">Facebook</RouterLink>
-    <RouterLink to="/email" class="text-m px-8 text-center text-sky-800">Email</RouterLink>
-    <RouterLink to="/twitter" class="text-m px-8 text-end text-sky-800">Twitter</RouterLink>
+    <RouterLink to="/facebook" class="text-m px-8 text-slate-800">Facebook</RouterLink>
+    <RouterLink to="/email" class="text-m px-8 text-slate-800">Email</RouterLink>
+    <RouterLink to="/twitter" class="text-m px-8 text-center text-slate-800">Twitter</RouterLink>
+    <p class="text-m px-2 text-end text-slate-800">Phone: 555-555</p>
     <p class="text-center text-sm text-sky-900">© Book Generator</p>
   </footer>
 </template>
