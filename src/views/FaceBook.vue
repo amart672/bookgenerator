bdbd9f3e803663e4fa051cb0b545c5141fb08be6
amart672 <amart672@stu.southtexascollege.edu>
2022-12-06 22:07:26 -0600

    resized the logos

--- src/views/FaceBook.vue
+++ src/views/FaceBook.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>This is the Facebook Page</h1>
-  <img src="/pictures/Facebook_icon.png" atl="facebook logo" class="h-28 w-28" />
+  <img src="/pictures/Facebook_icon.png" atl="facebook logo" class="h-80 w-80" />
 </template>
--- src/views/TwitterPage.vue
+++ src/views/TwitterPage.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>This is the Twitter Page</h1>
-  <img src="/pictures/twitter.png" alt="Twitter Logo" />
+  <img src="/pictures/twitter.png" alt="Twitter Logo" class="h-80 w-80" />
 </template>
